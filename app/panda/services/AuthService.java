9a4f2000fd33d6f9fa5252b42c3515a7fe33b339
99x-chinthaka <chinthakadi@99x.lk>
2019-01-10 20:14:14 +0530

    auth filter + jwt

--- app/panda/services/AuthService.java
+++ app/panda/services/AuthService.java
@@ -4,10 +4,14 @@ import java.io.UnsupportedEncodingException;
 import java.time.ZoneId;
 import java.time.ZonedDateTime;
 import java.util.Date;
+import java.util.List;
+import java.util.Objects;
+import java.util.stream.Collectors;
 
 import javax.inject.Inject;
 import javax.inject.Singleton;
 
+import be.objectify.deadbolt.java.models.Role;
 import com.auth0.jwt.JWT;
 import com.auth0.jwt.algorithms.Algorithm;
 import com.auth0.jwt.exceptions.JWTCreationException;
@@ -22,91 +26,76 @@ import play.inject.ApplicationLifecycle;
 
 @Singleton
 public class AuthService extends BaseService {
-	
-	
-	@Inject
-	public AuthService (Environment env, Config conf, ApplicationLifecycle al) { //(Environment env, Config conf, ApplicationLifecycle al, Application app) {
-		super(env, conf, al); // TODO: Fix
-		//this.environment = env;
-/*
-		String globalSalt = conf.getString("ion.userService.globalSalt");
-		if(globalSalt==null || globalSalt.isEmpty()) {
-			Logger.info("Loading default salt. For production, salt must come from production configuration file");
-			globalSalt = "01b6665c-1eca-433c-9add-58268cdfb0ea";
-		} 
-		
-		BaseUser.setGlobalSalt(globalSalt);*/
-		Logger.info("Global salt set");
-	}
-	
-//	public AuthenticatedUser authenticate(LoginForm loginAttempt) {
-//		authenticateUser(loginAttempt.getUsername());
-//		return null;
-//	}
-	
-//  return JWT.create()
-//  .withIssuer("ThePlayApp")
-//  .withClaim("user_id", userId)
-//  .withExpiresAt(Date.from(ZonedDateTime.now(ZoneId.systemDefault()).plusMinutes(10).toInstant()))
-//  .sign(algorithm);
-	
-	public AuthenticatedUser authenticateLoginAttempt(LoginForm loginAttempt) {
-		
-		return authenticateUser(loginAttempt.getUsername(),loginAttempt.getPassword());
-	}
-	
-	public AuthenticatedUser authenticateUser(String email, String enteredPassword) {// { throws AuthenticationException {
-		email = email.toLowerCase();
-		AuthenticatedUser user = AuthenticatedUser.findUserByEmail(email);
-		if(user==null) { // || user.isDeleted()) {
-	//		AuthenticationException e = new AuthenticationException("User does not exist");
-	//		LoginHistory lh = new LoginHistory(email, enteredPassword, LoginHistory.FAIL,e.getMessage());
-	//		Ebean.save(lh);
-			//throw e;
-			return null;
-		}
-				
-		if(user.isDisabledByAdmin() || user.isDisabledByUser() ) {
-	//		AuthenticationException e = new AuthenticationException("User is disabled");
-	//		LoginHistory lh = new LoginHistory(email, enteredPassword, LoginHistory.FAIL,e.getMessage());
-	//		Ebean.save(lh);
-			return null;
-			//throw e;
-		}
-		
-		if(!user.authenticate(enteredPassword)) {
-	//		AuthenticationException e = new AuthenticationException("User or password mismatch");
-	//		LoginHistory lh = new LoginHistory(email, enteredPassword, LoginHistory.FAIL,e.getMessage());
-	//		Ebean.save(lh);
-	//		throw e;
-			return null;
-	}
-			
-	//LoginHistory lh = new LoginHistory(email, enteredPassword, LoginHistory.SUCCESS,"");
-	//Ebean.save(lh);
-	user.setLastLogin(new Date());
-	Ebean.update(user);
-	return user;
-}
-	
-	
-	public String generateJWTToken(AuthenticatedUser user) {
-		String token = "";
-		String secret = conf.getString("play.http.secret.key");
-		try {
-		    Algorithm algorithm = Algorithm.HMAC256(secret);
-		    token = JWT.create()
-		        .withIssuer("airrefer.com")
-		        .withClaim("email", user.getEmail())
-		        .withClaim("jti", user.getJtiToken())
-		        .withExpiresAt(Date.from(ZonedDateTime.now(ZoneId.systemDefault()).plusMinutes(180).toInstant()))
-		        .sign(algorithm);
-		} catch (UnsupportedEncodingException exception){
-		    //UTF-8 encoding not supported
-		} catch (JWTCreationException exception){
-		    //Invalid Signing configuration / Couldn't convert Claims.
-		}
-		
-		return token;
-	}
+
+    @Inject
+    public AuthService(Environment env, Config conf, ApplicationLifecycle al) {
+        super(env, conf, al);
+        Logger.info("Global salt set");
+    }
+
+    public AuthenticatedUser authenticateLoginAttempt(LoginForm loginAttempt) {
+
+        return authenticateUser(loginAttempt.getUsername(), loginAttempt.getPassword());
+    }
+
+    public AuthenticatedUser authenticateUser(String email, String enteredPassword) {
+        email = email.toLowerCase();
+        AuthenticatedUser user = AuthenticatedUser.findUserByEmail(email);
+        if (user == null) {
+            return null;
+        }
+
+        if (user.isDisabledByAdmin() || user.isDisabledByUser()) {
+            return null;
+        }
+
+        if (!user.authenticate(enteredPassword)) {
+            return null;
+        }
+
+        user.setLastLogin(new Date());
+        Ebean.update(user);
+        return user;
+    }
+
+
+    public String generateJWTToken(AuthenticatedUser user) {
+        String token = "";
+        String secret = conf.getString("play.http.secret.key");
+
+        //Reading user roles as string array to pass with JWT token.
+        List<String> userRoleNames = user.getRoles().stream()
+                .filter(Objects::nonNull)
+                .map(Role::getName)
+                .collect(Collectors.toList());
+        try {
+            Algorithm algorithm = Algorithm.HMAC256(secret);
+            token = JWT.create().withKeyId("airrefer.com")
+                    .withIssuer("airrefer.com")
+                    .withClaim("email", user.getEmail())
+                    .withClaim("jti", user.getJtiToken())
+                    //Binding profile picture
+                    .withClaim("profile_image", user.getPicURL())
+                    //Binding user roles
+                    .withArrayClaim("roles", userRoleNames.stream().toArray(String[]::new))
+                    //.withExpiresAt(Date.from(ZonedDateTime.now(ZoneId.systemDefault()).plusHours(4).toInstant()))
+                    .withExpiresAt(Date.from(ZonedDateTime.now(ZoneId.systemDefault()).plusMinutes(2).toInstant()))
+                    .sign(algorithm);
+        } catch (UnsupportedEncodingException exception) {
+            //UTF-8 encoding not supported
+        } catch (JWTCreationException exception) {
+            //Invalid Signing configuration / Couldn't convert Claims.
+        }
+
+        return token;
+    }
+
+    public String readRoles(List<? extends Role> roles) {
+        StringBuilder stringBuilder  = new StringBuilder();
+        roles.forEach(role->{
+            stringBuilder.append(role.getName());
+            stringBuilder.append(",");
+        });
+        return stringBuilder.toString();
+    }
 }
--- app/security/panda/jwt/JwtValidator.java
+++ app/security/panda/jwt/JwtValidator.java
@@ -5,7 +5,8 @@ import play.libs.F;
 public interface JwtValidator {
 
     enum Error {
-        ERR_INVALID_SIGNATURE_OR_CLAIM
+        ERR_INVALID_SIGNATURE_OR_CLAIM,
+        TOKEN_EXPIRED
     }
 
     F.Either<Error, VerifiedJwt> verify(String token);
